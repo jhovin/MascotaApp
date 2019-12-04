f0db5ef8d407cae41d4558e987c31ce8d8fadf4a
Alumno <Alumno@LAB1507-20>
2019-12-04 18:03:40 -0500

    first changes

--- mascotas-api/src/main/java/pe/aplicacion/app/controllers/UsuarioController.java
+++ mascotas-api/src/main/java/pe/aplicacion/app/controllers/UsuarioController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -29,7 +30,11 @@ public class UsuarioController {
 		
 		return productos;
 	}
+	@GetMapping("/usuarios/{id}")
+	private Usuario obtener(@PathVariable("id")Long id) {
+	Usuario usuario=usuarioService.findById(id);
+	return usuario;
 	
-
+	}
 	
 }
